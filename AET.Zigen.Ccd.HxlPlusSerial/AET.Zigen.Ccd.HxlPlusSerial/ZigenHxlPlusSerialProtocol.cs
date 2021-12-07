21843986fe86eabd3e16c8fc7c8d64aa962b4548
Tony Evert <tony722@gmail.com>
2021-12-06 16:57:20 -0800

    Add audio routing

--- AET.Zigen.Ccd.HxlPlusSerial/AET.Zigen.Ccd.HxlPlusSerial/ZigenHxlPlusSerialProtocol.cs
+++ AET.Zigen.Ccd.HxlPlusSerial/AET.Zigen.Ccd.HxlPlusSerial/ZigenHxlPlusSerialProtocol.cs
@@ -4,6 +4,7 @@ using Crestron.RAD.Common.Enums;
 using Crestron.RAD.Common.Transports;
 using Crestron.RAD.DeviceTypes.AudioVideoSwitcher;
 using Crestron.SimplSharp;
+using Crestron.SimplSharpPro.DM;
 
 namespace AET.Zigen.Ccd.HxlPlusSerial {
   public class ZigenHxlPlusSerialProtocol : AAudioVideoSwitcherProtocol {
@@ -12,11 +13,11 @@ namespace AET.Zigen.Ccd.HxlPlusSerial {
       base.PowerIsOn = true;      
     }
 
+    /*
     protected override ValidatedRxData ResponseValidator(string response, CommonCommandGroupType commandGroup) {
       return base.ResponseValidator(response, commandGroup);
     }
 
-    /*
     public override void RouteVideoInput(string inputId, string outputId) {
       var cmdText = string.Format("Overrride Switch Input {0} to {1}", inputId, outputId);
       var command = new CommandSet("AudioVideoSwitcherRoute", cmdText, CommonCommandGroupType.AudioVideoSwitcher, null, false, CommandPriority.Normal, StandardCommandsEnum.AudioVideoSwitcherRoute);
@@ -25,10 +26,31 @@ namespace AET.Zigen.Ccd.HxlPlusSerial {
      */
 
     protected override bool PrepareStringThenSend(CommandSet commandSet) {
-      if (base.EnableLogging) base.Log("Zigen HXL TX$: '" + commandSet.Command + "'");
+      if (EnableLogging) Log("Zigen HXL TX$: '" + commandSet.Command + "'");
+      //if (commandSet.StandardCommand == StandardCommandsEnum.AudioVideoSwitcherRoute) {
+      //  CreateAVRouteCommand(commandSet);
+      //}
       commandSet.Command += "\n";
-      return base.PrepareStringThenSend(commandSet);      
+      return base.PrepareStringThenSend(commandSet);
     }
-    
+
+    private void CreateAVRouteCommand(CommandSet commandSet) {
+      const int firstAudioOutputNumber = 11;
+      int input;
+      int output;
+      try {
+        var route = commandSet.Command.Split(',');
+        input = int.Parse(route[0]);
+        output = int.Parse(route[1]);
+      }
+      catch (Exception ex) {
+        if (EnableLogging) Log(string.Format("Zigen HXL CreateAVRouteCommand: Error parsing input and output numbers from '{1}',. {0}", ex.Message, commandSet.Command));
+        input = 0;
+        output = 0;
+      }
+      var newCommand = string.Format(output < firstAudioOutputNumber ? "switch video {0} {1}" : "switch audio {0} {1}", output, input);
+      if (EnableLogging) Log(string.Format("Zigen HXL CreateAVRouteCommand({0}): Input = {1}, Output = {2}. New command = {3}.", commandSet.Command, input, output, newCommand));
+      commandSet.Command = newCommand;
+    }
   }
 }
